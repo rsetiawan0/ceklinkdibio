c99df063cf07beb5eef6ba54da8d845ed5b6adff
rsetiawan0 <41310002+rsetiawan0@users.noreply.github.com>
2026-02-10 20:23:19 +0700

    Update ga-init.js
    Signed-off-by: rsetiawan0 <41310002+rsetiawan0@users.noreply.github.com>

--- js/ga-init.js
+++ js/ga-init.js
@@ -1,4 +1,4 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-E0RPT2H34M');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-E0RPT2H34M');
